bd89f92c7056bbf06b932b88a7725062a87c54c2
humy06001 <humy06001@E-PW0234KS>
2023-10-18 12:43:00 +0200

    Add project files.

---
+++ Kap 4 Uppgift 4.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i<=100; i++) 
+            {
+                if (i % 3 == 0 && i % 5 == 0) //delbart med 3 och 5?
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0) //delbart med 5?
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0) //Delbart med 3?
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
